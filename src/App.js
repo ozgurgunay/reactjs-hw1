2a787e8c01538155a323d70b04db91fb02b50b15
ozgurgunay <gnyozgur@yandex.com>
2021-09-14 18:17:35 +0300

    menu

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
+
+
+function App() {
+  return (
+    <>
+     <Navbar></Navbar>
+     <Header></Header>
+     <Content></Content>
+     <Footer></Footer>
+    </>
+
+  )
+}
+
+export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import About from './About'
+import Contact from './Contact'
+import Menu from './Menu'
+
+
+function Content() {
+    return (
+        <>
+            {/* Page content */}
+            <div className="w3-content" style={{ maxWidth: 1100 }}>
+                <About></About>
+                <Menu></Menu>
+                <Contact></Contact>
+
+            </div>
+        </>
+
+    )
+}
+
+export default Content
